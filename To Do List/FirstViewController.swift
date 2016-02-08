c708da92ec15eba4932541c384736e0e2e3f8835
venkat <vikkii89@gmail.com>
2016-02-08 14:05:58 -0800

    ios app for todo list

--- To Do List/FirstViewController.swift
+++ To Do List/FirstViewController.swift
@@ -8,17 +8,59 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+var toDoList = [String]()
 
+class FirstViewController: UIViewController, UITableViewDelegate {
+
+    @IBOutlet weak var toDoListTable: UITableView!
+    
+ 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        if NSUserDefaults.standardUserDefaults().objectForKey("toDoList") != nil {
+            
+            toDoList = NSUserDefaults.standardUserDefaults().objectForKey("toDoList") as! [String]
+        }
+        
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    internal func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return toDoList.count
+    }
+    
+   
+    
+    @available(iOS 2.0, *)
+    internal func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        cell.textLabel?.text = toDoList[indexPath.row]
+            return cell
+    }
+
+    override func viewDidAppear(animated: Bool) {
+        toDoListTable.reloadData()
+    }
+    
+    internal func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        
+        if editingStyle == UITableViewCellEditingStyle.Delete {
+            toDoList.removeAtIndex(indexPath.row)
+            
+            NSUserDefaults.standardUserDefaults().setObject(toDoList, forKey: "toDoList")
+            toDoListTable.reloadData()
+        }
+    }
+    
 
 
 }
--- To Do List/SecondViewController.swift
+++ To Do List/SecondViewController.swift
@@ -8,16 +8,36 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController , UITextFieldDelegate{
 
+    @IBOutlet weak var inpVal: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.inpVal.delegate  = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func addItem(sender: AnyObject) {
+        
+        toDoList.append(inpVal.text!)
+        inpVal.text = ""
+        
+        NSUserDefaults.standardUserDefaults().setObject(toDoList, forKey: "toDoList")
+        
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
 
 
